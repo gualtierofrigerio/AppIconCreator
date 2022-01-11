e011a8a9f19000c97d3c0cac09b832446357c872
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-11 17:27:39 +0100

    added changeDestination

--- AppIconCreator/Utils/FileUtils.swift
+++ AppIconCreator/Utils/FileUtils.swift
@@ -5,11 +5,33 @@
 //  Created by Gualtiero Frigerio on 09/01/22.
 //
 
+import AppKit
 import Foundation
 
 class FileUtils {
     static var defaultDestinationURL: URL? {
         try? getUserDownloadsURL()
+    }
+    
+    class func changeDestination(completion: @escaping (URL?) -> Void) {
+        let dialog = NSOpenPanel();
+        
+        dialog.title                    = "Choose a directory"
+        dialog.allowsMultipleSelection  = false
+        dialog.canChooseDirectories     = true
+        dialog.canChooseFiles           = false
+        dialog.canCreateDirectories     = true
+        dialog.showsResizeIndicator     = true
+        dialog.showsHiddenFiles         = false
+        
+        if (dialog.runModal() ==  NSApplication.ModalResponse.OK) {
+            if let url = dialog.url {
+                completion(url)
+            }
+        } else {
+            print("user cancelled")
+            completion(nil)
+        }
     }
     
     class func getUserDownloadsURL() throws -> URL {
