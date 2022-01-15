cafd068eedc75e2ff39002ad79c868e2e63ecf78
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-15 13:58:38 +0100

    added Error

---
+++ AppIconCreator/Utils/Errors.swift
@@ -0,0 +1,34 @@
+//
+//  Errors.swift
+//  AppIconCreator
+//
+//  Created by Gualtiero Frigerio on 11/01/22.
+//
+
+import Foundation
+
+enum IconCreatorError: Error {
+    case loadImage
+    case resize
+    case save
+}
+
+extension IconCreatorError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .loadImage:
+            return NSLocalizedString("Error loading original image", comment: "")
+        case .resize:
+            return NSLocalizedString("Error resizing images", comment: "")
+        case .save:
+            return NSLocalizedString("Error saving icons", comment: "")
+        }
+    }
+}
+
+enum Warnings: String {
+    case cannotLoadImage = "Cannot load image from the file you dropped"
+    case imageInvalidFormat = "The image is in an invalid format"
+}
+
+
